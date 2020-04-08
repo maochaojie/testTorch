7b73afaac2b24f5052e1bb7e5f880b7c232fe96c
mcj <duanmurs@163.com>
2020-04-09 01:03:13 +0800

    update

--- torch_data/vecDataList.py
+++ torch_data/vecDataList.py
@@ -1,10 +1,14 @@
 import numpy as np
 import torch
+import json
 class VecDataset(torch.utils.data.Dataset):
-    def __init__(self, file_path, pre_length = 7):
+    def __init__(self, file_path, stage, future = 1, pre_length = 7, test_num = 50):
         self.all_data = []
         self.file_path = file_path
         self.pre_length = pre_length
+        self.test_num = test_num
+        self.future = future
+        self.stage = stage
         self.collect_data()
 
     def collect_data(self):
@@ -21,19 +25,46 @@ class VecDataset(torch.utils.data.Dataset):
             price = line.split('#:#')[0]
             price_list.append(float(price))
             index_time += 1
-        all_price_vec = np.array(price_list)
-        for idx in range(len(price_list)-1):
-            price_vec = np.zeros([self.pre_length], dtype = np.float32)
-            if idx < self.pre_length:
-                price_vec[self.pre_length - idx:] = all_price_vec[0:idx]
-            else:
-                price_vec[...] = all_price_vec[idx - self.pre_length:idx]
-            predict_val = all_price_vec[idx]
-            self.all_data.append([price_vec, predict_val])
+        all_price_vec = np.array(price_list[:-self.test_num])
+        self.mean = np.mean(all_price_vec)
+        self.std = np.std(all_price_vec)
+        meta_json = {}
+        meta_json['mean'] = self.mean
+        meta_json['std'] = self.std
+        json_str = json.dumps(meta_json)
+        with open('./resource/sta.json', 'w') as f:
+            f.write(json_str)
+        if self.stage == 'train':
+            print("data mean:{}, std:{}".format(self.mean, self.std))
+            all_price_vec = (all_price_vec - self.mean) / self.std
+            for idx in range(all_price_vec.shape[-1]-self.future):
+                price_vec = np.zeros([self.pre_length], dtype = np.float32)
+                predict_val = np.zeros([self.pre_length + self.future - 1], dtype = np.float32)
+                if idx < self.pre_length:
+                    price_vec[self.pre_length - idx:] = all_price_vec[0:idx]
+                    predict_val[self.pre_length - idx:] = all_price_vec[1:idx+self.future]
+                else:
+                    price_vec[...] = all_price_vec[idx - self.pre_length:idx]
+                    predict_val[...] = all_price_vec[idx - self.pre_length + 1:idx + self.future]
+                self.all_data.append([price_vec, predict_val])
+        else:
+            print("data mean:{}, std:{}".format(self.mean, self.std))
+            all_price_vec = np.array(price_list[-self.test_num-self.pre_length:])
+            all_price_vec = (all_price_vec - self.mean) / self.std
+            for idx in range(self.pre_length, all_price_vec.shape[-1]- self.future, 1):
+                price_vec = np.zeros([self.pre_length], dtype=np.float32)
+                predict_val = np.zeros([self.pre_length + self.future - 1], dtype=np.float32)
+                if idx < self.pre_length:
+                    price_vec[self.pre_length - idx:] = all_price_vec[0:idx]
+                    predict_val[self.pre_length - idx:] = all_price_vec[1:idx + self.future]
+                else:
+                    price_vec[...] = all_price_vec[idx - self.pre_length:idx]
+                    predict_val[...] = all_price_vec[idx - self.pre_length + 1:idx + self.future]
+                self.all_data.append([price_vec, predict_val])
 
     def __getitem__(self, index):
         feature, predict_val =  self.all_data[index]
-        return torch.from_numpy(feature), predict_val/10000
+        return torch.from_numpy(feature), predict_val
 
     def __len__(self):
         return len(self.all_data)
--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import numpy as np
 from torch import nn
+
+from tools.tool import plot_curve
 from torch_data import vecDataList
 import torch
 import sys
@@ -30,26 +32,63 @@ class LSTMLR(nn.Module):
         # out = self.relu(out)
         out = self.fc2(ls[-1, :, :])
         return out
+class Sequence(nn.Module):
+    def __init__(self, feature_size = 64):
+        super(Sequence, self).__init__()
+        self.lstm1 = nn.LSTMCell(1, feature_size)
+        self.lstm2 = nn.LSTMCell(feature_size, feature_size)
+        self.linear = nn.Linear(feature_size, 1)
+        self.feature_size = feature_size
+    def forward(self, input, future = 1):
+        future = future - 1
+        h_t = torch.zeros(input.size(0), self.feature_size, dtype= torch.float32)
+        c_t = torch.zeros(input.size(0), self.feature_size, dtype=torch.float32)
+        h_t2 = torch.zeros(input.size(0), self.feature_size, dtype=torch.float32)
+        c_t2 = torch.zeros(input.size(0), self.feature_size, dtype=torch.float32)
+        outputs = []
+        for i, input_t in enumerate(input.chunk(input.size(1), dim = 1)):
+            h_t , c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+        for i in range(future):
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs += [output]
+        outputs = torch.stack(outputs, 1).squeeze(2)
+        return outputs
+
+
 ###
 class MSE(nn.Module):
     def __init__(self):
         super(MSE, self).__init__()
-        self.ce = nn.MSELoss()
+        self.ce = nn.MSELoss(size_average = False, reduce = False)
     def forward(self, input, target):
         loss = self.ce(input, target)
-        return loss
+        loss = torch.sum(loss, dim = -1)
+        return loss.mean()
 
+pre_length = 14
+# 预测未来多少天
+future = 5
 if __name__ == '__main__':
     file_path = './resource/price_file.csv'
-    pre_length = 14
-    stage = 'train'
+    stage = 'test'
     num_workers = 2
     learning_rate = 0.1
-    mom = 0.9
     weight_decay = 0.0005
     all_epoches = 50
-    batch_size = 100
-    dataClass = vecDataList.VecDataset(file_path, pre_length = pre_length)
+    save_step = 20
+    upload_step = 50
+    batch_size = 20
+
+
+    if stage == 'test':
+        batch_size = 2
+
+    dataClass = vecDataList.VecDataset(file_path, stage = stage, pre_length = pre_length, future=future, test_num=100)
     dataLoader = torch.utils.data.DataLoader(dataset = dataClass,
                                              batch_size = batch_size,
                                              shuffle = (stage == 'train'),
@@ -57,47 +96,75 @@ if __name__ == '__main__':
                                              pin_memory = True,
                                              drop_last = True)
 
-    model = LSTMLR(1)
+    model = Sequence()
     criterion = MSE()
     params_list = [{"params": model.parameters()}]
-    optimizer = torch.optim.SGD(params_list, lr = learning_rate, momentum=mom, weight_decay=weight_decay)
+    optimizer = torch.optim.Adam(params_list, lr = learning_rate,  weight_decay=weight_decay)
 
-    stage = 'train'
     if stage == 'train':
         model.train()
-
         eiters = 0
         for epoch in range(all_epoches):
             for i, train_data in enumerate(dataLoader):
                 feature, target = train_data
-                feature = torch.reshape(feature, [-1, pre_length, 1]).permute(1, 0, 2)
-                target = torch.reshape(target, [-1, 1])
                 out = model(feature)
                 loss = criterion(out, target)
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
                 eiters += 1
-                if eiters%2 == 0:
+                if eiters%10 == 0:
                     print("epoch:{} eiter: {} loss: {}".format(epoch, eiters, loss.item()))
-            if (epoch+1) % 10 == 0 :
+            if (epoch+1) % upload_step == 0 :
                 lr = learning_rate * (0.1 ** ((epoch+1)//10))
                 for param_group in optimizer.param_groups:
                     param_group['lr'] = lr
-            if epoch % 5 == 0:
+            if epoch % save_step == 0:
                 state = {'model':model,
                          'model_state':model.state_dict(),
                          'optimizer':optimizer.state_dict()}
                 prefix_path = './resource/{}_checkpoint.pth.tar'.format(epoch)
                 torch.save(state, prefix_path)
+        torch.save(state, prefix_path)
     else:
-        epoch = 5
+        epoch = 40
+        all_loss = 0
+        out_list = []
+        target_list = []
         prefix_path = './resource/{}_checkpoint.pth.tar'.format(epoch)
         checkpoint = torch.load(prefix_path, map_location='cpu')
         model.load_state_dict(checkpoint['model_state'])
         for i, train_data in enumerate(dataLoader):
             feature, target = train_data
-            feature = torch.reshape(feature, [-1, pre_length, 1])
-            target = torch.reshape(target, [-1, 1])
             out = model(feature)
-            print(target*10000, out*10000)
+            loss = criterion(out[:,-future], target[:, -future])
+            all_loss += loss.item()
+            out = out[:, -future] * dataClass.std + dataClass.mean
+            target = target[:, -future] * dataClass.std + dataClass.mean
+            target = target.detach().cpu().numpy()
+            output = out.detach().cpu().numpy()
+            for index in range(target.shape[0]):
+                target_list.append(target[index])
+                out_list.append(output[index])
+                print("target: {} output:{}".format(target[index], output[index]))
+            name_list =['target', 'predict']
+            recall_list = [[v+1 for v in range(len(target_list))], [v+1 for v in range(len(target_list))]]
+            precision_list = [target_list, out_list]
+            plot_path = './resource/price_predict.png'
+            plot_curve(name_list, recall_list, precision_list, plot_path, sample_num=len(target_list))
+        # up or down
+        next_target_list = target_list[1:]
+        target_list = target_list[:-1]
+
+        next_out_list = out_list[1:]
+        out_list = out_list[:-1]
+
+        target_is_up_down = np.array(next_target_list) - np.array(target_list)
+        out_is_up_down = np.array(next_out_list) - np.array(out_list)
+
+        target_is_up_down = target_is_up_down > 0
+        out_is_up_down = out_is_up_down > 0
+
+        right_num = np.sum(target_is_up_down * out_is_up_down) + np.sum((1 - target_is_up_down) * (1 - out_is_up_down))
+        print('up down precision: {} loss: {}'.format(right_num/target_is_up_down.shape[-1], all_loss/target_is_up_down.shape[-1]))
+
